2f8592baed4c34be6d9f2f03fbf3984c8fcd8288
guumaster <gustavoandresmarin@gmail.com>
2014-09-21 13:57:33 +0200

    added timezone information

--- bin/roa.js
+++ bin/roa.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 var roa = require('../lib');
+var moment = require('moment-timezone');
 
 roa.get({
     locale: 'es'
 }, function(err, time){
     if( err ) { console.log( err ); return; }
 
-    console.log( 'ROA time is: ', time.format('LLLL') );
+    console.log( 'ROA  time is: ', time.tz('Europe/Madrid').format() );
+    console.log( 'your time is: ', moment().format() );
 });
